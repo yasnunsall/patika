455147e1d68914ff420914c07f6ca4e6a97e736c
yasnunsall <111451338+yasnunsall@users.noreply.github.com>
2022-12-07 21:47:18 +0300

    Create ödev5.sql

---
+++ ödev5.sql
@@ -0,0 +1,5 @@
+--1. select title from film where title like '%n' order by length desc limit 5;
+
+--2. select title from film where title like '%n' order by length asc offset 5 limit 5;
+
+--3. select * from customer where store_id = 1 order by last_name desc limit 4;
