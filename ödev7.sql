e8d4dc15adcdf89165f40b2a539475f451cf96bd
yasnunsall <111451338+yasnunsall@users.noreply.github.com>
2022-12-07 22:36:41 +0300

    Create ödev7.sql

---
+++ ödev7.sql
@@ -0,0 +1,13 @@
+--1
+/* select count(*), rating from film group by rating; */
+
+--2
+/* select replacement_cost, count(*) from film 
+group by replacement_cost having count(*) > 50 order by count(*) desc; */
+
+--3
+/* select store_id, count(*) from customer group by store_id ; */
+
+--4
+/* select country_id, count(*) from city group by country_id
+order by count(*) desc limit 1; */
