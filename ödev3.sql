c8b24892e0f0eae9cf5266d6932da65daf9e9215
yasnunsall <111451338+yasnunsall@users.noreply.github.com>
2022-12-07 14:38:53 +0300

    Create ödev3.sql

---
+++ ödev3.sql
@@ -0,0 +1,7 @@
+--1. select country from country where country ilike 'A%a';
+
+--2. select country from country where length(country)>=6 and country ilike '%n';
+
+--3. select title from film where title like '%t%t%t%t%';
+
+--4. select * from film where length > 90 and title ilike 'C%' and rental_rate = 2.99;
