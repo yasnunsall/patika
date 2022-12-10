c48da31e1bb1e0216dc572b8b0523b41df2739d9
yasnunsall <111451338+yasnunsall@users.noreply.github.com>
2022-12-10 10:04:34 +0300

    Update ödev3.sql

--- ödev3.sql
+++ ödev3.sql
@@ -1,7 +1,11 @@
---1. select country from country where country like 'A%a';
+--1
+select country from country where country like 'A%a';
 
---2. select country from country where country like '______%' and country like '%n';
+--2
+select country from country where country like '______%' and country like '%n';
 
---3. select title from film where title like '%t%t%t%t%';
+--3
+select title from film where title like '%t%t%t%t%';
 
---4. select * from film where length > 90 and title like 'C%' and rental_rate = 2.99;
+--4
+select * from film where length > 90 and title like 'C%' and rental_rate = 2.99;
