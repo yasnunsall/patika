9b88504114e511d9aebc29576cf0d8aa0248efab
yasnunsall <111451338+yasnunsall@users.noreply.github.com>
2022-12-10 10:05:00 +0300

    Update ödev4.sql

--- ödev4.sql
+++ ödev4.sql
@@ -1,9 +1,14 @@
---1. select distinct replacement_cost from film;
+--1
+select distinct replacement_cost from film;
 
---2. select count(distinct replacement_cost) from film;
+--2
+select count(distinct replacement_cost) from film;
 
---3. select count(title) from film where title like 'T%' and rating = 'G';
+--3
+select count(title) from film where title like 'T%' and rating = 'G';
 
---4. select count(country) from country where country like '_____';
+--4
+select count(country) from country where country like '_____';
 
---5. select count(city) from city where city like 'R%' or city like '%r';
+--5
+select count(city) from city where city like 'R%' or city like '%r';
