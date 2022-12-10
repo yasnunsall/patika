e00679140b0a5dbc6ed06213f39e47417df7b3b2
yasnunsall <111451338+yasnunsall@users.noreply.github.com>
2022-12-10 10:03:51 +0300

    Update ödev1.sql

--- ödev1.sql
+++ ödev1.sql
@@ -1,10 +1,15 @@
+--1
 select title, description from film;
 
+--2
 select * from film where length > 60 and length < 75 order by length desc;
 
+--3
 select * from film where rantal_rate = 0.99 and ( replacement_cost = 12.99 or replacement_cost = 28.99);
 
--select last_name from customer where first_name = "Mary";
+--4
+select last_name from customer where first_name = "Mary";
 
+--5
 select * from film where length <= 50 and not ( rental_rate = 2.99 or rental_rate = 4.99) ;
 
